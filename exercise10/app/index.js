ccec0330e7c3e95284ad438481a53f8e6335b1c8
Kullister <kullmann.kulli@web.de>
2017-03-17 10:54:59 +0100

    index erweitert

--- exercise10/app/index.js
+++ exercise10/app/index.js
@@ -1,5 +1,38 @@
+module.exports = function(passport, app, router) {
 
+  app.get('/', function(req, res) {
+    res.render('index', { title: 'Express' });
+  });
 
+  app.get('/login', function(req, res) {
+    res.render('login', { title: 'Login' });
+  });
+
+  app.post('/dologin',
+    passport.authenticate('local-login', {
+      successRedirect : '/loginuser',
+      failureRedirect : '/login'
+    })
+  );
+
+  app.get('/loginuser', isLoggedIn, function(req, res) {
+    res.render('userstart', { user: req.user });
+  });
+
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/login');
+  });
+
+};
+
+function isLoggedIn(req, res, next) {
+
+  if (req.isAuthenticated())
+    return next();
+
+  res.redirect('/login');
+}
 
 
 
