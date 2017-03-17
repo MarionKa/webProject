2c770e199b896ecceaa62df7b5041b85cae04e83
Kullister <kullmann.kulli@web.de>
2017-03-17 09:35:57 +0100

    index.js

---
+++ exercise10/app/index.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+/* Einbinden der Dateien */
+src="lib/es6-shim.min.js"
+src="lib/jquery-1.12.0.min.js"
+src="lib/bootstrap.min.js"
+src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"
+src="lib/angular.min.js""></script>"
+src="lib/angular-route.min.js"
+src="lib/ui-bootstrap-1.2.1.min.js"
+src="lib/material.min.js"
+src="scripts/app.js"
+src="scripts/Datum.js"
+src="scripts/Student.js"
+src="scripts/landung.js"
+src="controllers/BodyController.js"
+src="controllers/MoonController.js"
+src="controllers/PatientDetailsController.js"
+src="controllers/NavbarController.js"
+src="controllers/PatientListController.js"
+src="services/FhirService.js"
